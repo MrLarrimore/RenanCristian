b02cb8678860bbc488504a30e359eac7681bef16
Cristian <cristianrobles177@gmail.com>
2015-05-20 14:17:37 -0700

    added rating

--- index.php
+++ index.php
@@ -4,12 +4,14 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title>APPhub</title>
     </head>
     <body>
+        <table cellpadding="0" cellspacing="0" width="133" bgcolor=#FFFFFF><tr><td><iframe src="http://www.ratingcode.com/show.php?id=143208518763&st=4&bg=FFFFFF" border="0" width="115" height="37" marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no" bgcolor=FFFFFF></iframe></td><td width="18" align="center" valign="top" style="padding-top:5px"><a href="http://www.ratingcode.com" target="_blank"></a></td></tr></table>
         <?php
             
         ?>
